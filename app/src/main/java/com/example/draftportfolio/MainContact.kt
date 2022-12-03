267664b319531dfed069746963faa929286d8cdd
gen dee <jenniferdee86@gmail.com>
2022-12-03 20:31:47 +0000

    fourth activity added

--- app/src/main/java/com/example/draftportfolio/MainContact.kt
+++ app/src/main/java/com/example/draftportfolio/MainContact.kt
@@ -3,12 +3,9 @@ package com.example.draftportfolio
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainPortfolio : AppCompatActivity() {
+class MainContact : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mainmenu)
-
-
-
+        setContentView(R.layout.activity_main_contact)
     }
 }
