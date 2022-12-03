b06380999a462d92b5434812f410aba764e0159b
gen dee <jenniferdee86@gmail.com>
2022-12-03 14:31:24 +0000

    Second activity + onclick method

--- app/src/main/java/com/example/draftportfolio/MainActivity.kt
+++ app/src/main/java/com/example/draftportfolio/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.example.draftportfolio
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Button button = (Button) findViewById(R.id.button)
+        //button click event listener from main page
+        val btnClick = findViewById<Button>(R.id.button)
+        btnClick.setOnClickListener {
+            //toast
+            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT ).show()
+            val nextPage = Intent(this, portfolio::class.java)
+            startActivity(nextPage)
+        }
+
     }
+
 }
